2a7fc7091f5881dc45fc555ff1b017f60be18893
Arun Karthikeyan <akarthi2@asu.edu>
2016-07-14 19:27:12 -0700

    updated client code

--- client-dev/js/botapp.controllers.js
+++ client-dev/js/botapp.controllers.js
@@ -2,15 +2,24 @@
 
 angular.module('botapp', [])
 .controller("bot-controller", function($scope)  {
-    // $scope.submit = function()  {
-    //     var socket = window.io.connect("http://localhost:5000");
-    //     socket.emit("message", $scope.question);
-    //     socket.on("response", function(data)    {
-    //         alert(data);
-    //     });
-    // };
+    $scope.currentFormText = '';
+    $scope.messages = [{isTicky: true, isUser: false, text: 'Hi there, How may I help you today ?', time: new Date().getTime()}];
     $scope.tryTicky = function(){
       $('#tickey-desc').css('display','none');
       $('#tickey-chat').css('display','block');
+    };
+    var socket = window.io.connect("http://localhost:5000");
+    socket.on("response", function(data)    {
+        var newMessageObj = {isTicky: true, isUser: false, text: data.text, time: new Date().getTime()};
+        $scope.messages.push(newMessageObj);
+    });
+    $scope.sendUserMessage = function() {
+      if($scope.currentFormText.trim().length!=0)
+      {
+      var newMessageObj = {isTicky: false, isUser: true, text: $scope.currentFormText, time: new Date().getTime()};
+      $scope.messages.push(newMessageObj);
+      socket.emit("message", $scope.currentFormText);
+      $scope.currentFormText = '';
+      }
     };
 });
